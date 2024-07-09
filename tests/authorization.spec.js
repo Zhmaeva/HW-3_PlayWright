167c07f2ffe8173192a8550fd35994ff8ebb25df
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-07-09 23:15:03 +0300

    add tests

--- tests/authorization.spec.js
+++ tests/authorization.spec.js
@@ -3,31 +3,35 @@ const { chromium } = require("playwright");
 const {email, password, unvalidEmail, unvalidPass} = require("../user");
 const { beforeEach, afterEach, describe } = require("node:test");
 
-beforeEach(async () => {
-  page = await browser.newPage();
-  await page.goto("https://netology.ru");
-});
-afterEach(async () => {
-  await page.close();
-});
-
 describe("Authorization on the site Netology.ru", () => {
-  test ("Successful authorization", async () => {
-    // Click [data-testid="header-top"] div:has-text("Войти")
-  await page.click('[data-testid="header-top"] div:has-text("Войти")');
-  await page.click('text=Войти');
+  test ("Successful authorization", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
     // Click [placeholder="Email"]
-  await page.click('[placeholder="Email"]');
+  await page.getByPlaceholder('Email').click();
     // Fill [placeholder="Email"]
-  await page.fill('[placeholder="Email"]', email);
+  await page.getByPlaceholder('Email').fill(email);
+  await page.pause();
     // Click [placeholder="Пароль"]
-  await page.click('[placeholder="Пароль"]');
+  await page.getByPlaceholder('Пароль').click();
     // Fill [placeholder="Пароль"]
-  await page.fill('[placeholder="Пароль"]', password);
+  await page.getByPlaceholder('Пароль').fill(password);
     // Click [data-testid="login-submit-btn"]
-  await page.click('[data-testid="login-submit-btn"]');
+  await page.getByTestId('login-submit-btn').click();
+  await page.pause();
     // Expected
   await expect(page).toHaveURL("https://netology.ru/profile/8500991");
+  await expect(page.locator("h2")).toContainText("Моё обучение");
   });
 
+  test ("Authorization failed", async ({ page }) => {
+    await page.goto("https://netology.ru/?modal=sign_in");
+    await page.click('[placeholder="Email"]');
+    await page.fill('[placeholder="Email"]', unvalidEmail);
+    await page.click('[placeholder="Пароль"]');
+    await page.fill('[placeholder="Пароль"]', unvalidPass);
+    await page.click('[data-testid="login-submit-btn"]');
+
+    const error = await page.locator('[data-testid="login-error-hint"]');
+    await expect(error).toHaveText("Вы ввели неправильно логин или пароль.");
+    });
 });
